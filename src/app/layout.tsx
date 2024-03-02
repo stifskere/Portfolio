81c5db655376cabbb53f13ea80fec27bae0b4292
stifskere <stifskere@gmail.com>
2024-03-02 12:15:01 +0100

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,22 @@
-import type { Metadata } from "next";
+import type {Metadata, Viewport} from "next";
 import {ReactNode} from "react";
 
 export const metadata: Metadata = {
 	title: "Memw | Developer",
 	description: "Memw's portfolio.",
+	metadataBase: new URL("https://memw.es"),
+	openGraph: {
+		title: "Memw developer's portfolio",
+		type: "website",
+		url: "https://memw.es",
+		images: "/background.png",
+		description: "Discover how can I help you in your projects!"
+	}
 };
+
+export const viewport: Viewport = {
+	themeColor: "#000000"
+}
 
 export default function RootLayout({children}: Readonly<WithChildren<ReactNode>>) {
 	return (
